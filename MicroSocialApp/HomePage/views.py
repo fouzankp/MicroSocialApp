115f1eb0f2c3f4c2a27e806d5ae72fb523eaf925
fouzankp <muhammedfouzankp@gmail.com>
2022-10-08 18:34:26 +0530

    corrected issue with follow and redirecting

--- MicroSocialApp/HomePage/views.py
+++ MicroSocialApp/HomePage/views.py
@@ -23,20 +23,25 @@ def SignUp(request):
     return render(request, "loginorsignup.html", context)
 
 def LoginFeed(request):
-    print(request.POST)
+    #print(request.POST)
     userid1 = request.POST['userid']
     pswd = request.POST['pswd']
-    l = models.Login.objects.values('UserId')
-    print(l)
+    l = models.Login.objects.values('UserId','password')
     for i in l:
-        if userid1 in i.values():
+        if userid1 in i.values() and pswd in i.values():
             x = models.Login.objects.filter(UserId=userid1)
             fullname = x.values()[0].get('Fullname')
-            context = {'Username':fullname, 'Userid':userid1 }
-            return render(request, "Feed.html", context)
+            return FeedPage(request, userid1)
     context = {'Invalid': True }
     return render(request, "loginorsignup.html", context)
     
+def FeedPage(request, userid, followedid=''):
+    k = models.Login.objects.filter(UserId=userid)[0]
+    fullname = k.Fullname
+    print('here')
+    context = {'Username':fullname, 'Userid':userid, 'FollowedId':followedid }
+    return render(request, "Feed.html", context)
+
 
 def followuser(request):
     f = request.POST['follow']
@@ -45,11 +50,12 @@ def followuser(request):
     u = request.POST['userid']
     print(u)
     l = models.Login.objects.values('UserId')
+    print(l)
+    context = {'Followed':False, 'Userid':f  }
     for i in l:
         if f in i.values():
             p = models.Followers(UserId=u , FollowUserId=u+'-'+f)
             p.save()
+            return FeedPage(request, u, f)
             context = {'Followed':True, 'Userid':f }
-        else:
-            context = {'Followed':False, 'Userid':f  }
-    return render(request, "followuser.html", context)
+    return FeedPage(request, u, 'False')
